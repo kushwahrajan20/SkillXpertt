63dcb3d436efa48f5864d67eb7ecd4c768df2e1d
krajan2006 <kushawahaharikishan1@gmail.com>
2025-10-23 20:11:36 +0530

    update

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -73,7 +73,7 @@ export const login = async (req,res) => {
 }
 export const logout = async (_,res) => {
     try {
-        return res.status(200).cookie("token", "", {maxAge:0}).json({
+        return res.status(200).cookie("token", "", {maxAge:7 * 24 * 60 * 60 * 1000}).json({
             message:"Logged out successfully.",
             success:true
         })
